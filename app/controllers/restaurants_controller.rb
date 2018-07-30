8742963c9d8a74a61c35482a6a2bcb0173e0f8d8
allenpan <panjau@gmail.com>
2018-07-30 16:54:43 +0800

    Restaurant contrller add Kaminari order by restaurant id

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
 
   def index
-    @restaurants = Restaurant.page(params[:page]).per(9)
+    @restaurants = Restaurant.order(:id).page(params[:page]).per(9)
     @categories = Category.all 
   end
 
