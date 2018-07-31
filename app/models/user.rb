2c473876ce497c1d21fd431bad43e03a10828514
allenpan <panjau@gmail.com>
2018-07-31 18:13:45 +0800

    setup: user has many followrs

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,9 @@ class User < ApplicationRecord
   has_many :followships, dependent: :destroy
   has_many :followings, through: :followships
 
+  has_many :inverse_followships, class_name: "Followship", foreign_key: "following_id"
+  has_many :followers, through: :inverse_followships, source: :user
+
   validates_presence_of :name
   mount_uploader :avatar, AvatarUploader
 
