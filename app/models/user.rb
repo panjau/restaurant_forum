d4452233bd091e0b795b9702f19c569c51a0c24a
allenpan <panjau@gmail.com>
2018-08-07 01:24:16 +0800

    1.add has_many:confirmed_friendships,has_many:confirmed_friends, confirmed_inverse_friendships, confirmed_inverse_friends in the Model User, 2.implement all_friends function

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,10 +23,14 @@ class User < ApplicationRecord
   has_many :followers, through: :inverse_followships, source: :user
 
   has_many :friendships, dependent: :destroy
+  has_many :confirmed_friendships, -> { where confirmed: true}, primary_key: "id", foreign_key: "user_id", class_name: "Friendship", dependent: :destroy
   has_many :friends, through: :friendships
-
+  has_many :confirmed_friends, through: :confirmed_friendships, source: :friend
+  
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :confirmed_inverse_friendships, -> { where confirmed: true}, primary_key: "id", foreign_key: "friend_id", class_name: "Friendship", dependent: :destroy
   has_many :inverse_friends, through: :inverse_friendships, source: :user
+  has_many :confirmed_inverse_friends, through: :confirmed_inverse_friendships, source: :user
 
 
   validates_presence_of :name
@@ -53,8 +57,27 @@ class User < ApplicationRecord
   end
 
   def all_friends
-    friend_list = self.friends + self.inverse_friends
-   
+    # confirmed_friendships = self.friendships.where(confirmed: true)
+    # confirmed_inverse_friendships = self.inverse_friendships.where(confirmed: true)
+    # friend_list = Array.new
+    # if confirmed_friendships.count > 0
+    #   confirmed_friend_list = Array.new
+    #   confirmed_friendships.each do |friend|
+    #     confirmed_friend_list = confirmed_friend_list + self.friends.where(id: friend.friend_id) 
+    #   end
+    #   friend_list = friend_list + confirmed_friend_list
+    # end
+    # puts "confirmed_friend_list = #{confirmed_friend_list}"
+    
+    # if confirmed_inverse_friendships.count > 0
+    #   confirmed_inverse_friend_list = Array.new
+    #   confirmed_inverse_friendships.each do |friend|
+    #     confirmed_inverse_friend_list  = confirmed_inverse_friend_list  + self.inverse_friends.where(id: friend.user_id) 
+    #   end
+    #   friend_list = friend_list + confirmed_inverse_friend_list
+    # end
+    # puts "confirmed_inverse_friend_list = #{confirmed_inverse_friend_list }"
+    friend_list = self.confirmed_friends + self.confirmed_inverse_friends
     return friend_list
   end
 end
